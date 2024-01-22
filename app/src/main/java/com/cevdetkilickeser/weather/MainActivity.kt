aa66207b984a363b4c2c214c907506aaad48dc84
cevdetkilickeser <kilickesercevdet@gmail.com>
2024-01-22 20:34:07 +0300

    Initial commit, ux created

---
+++ app/src/main/java/com/cevdetkilickeser/weather/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cevdetkilickeser.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//df567c20b44a0b4626a5402ce5fccc4b
